a5810ffa0fa5079ce4f34549fdbf73728942ad3d
Mark Paluch <mpaluch@pivotal.io>
2017-11-09 14:18:15 +0100

    DATALDAP-5 - Add CDI extension for LDAP repositories.
    We now provide a CDI extension to create LDAP repositories in a CDI container. LDAP repositories require a LdapOperations bean as underlying resource manager for LDAP access.
    
    class LdapTemplateProducer {
    
      @Produces
      @Singleton
      public LdapTemplate createLdapTemplate() {
    
        LdapTemplate ldapTemplateMock = …
        return ldapTemplateMock;
      }
    }
    
    class RepositoryClient {
    
      @Inject
      PersonRepository repository;
    }

---
+++ src/main/java/org/springframework/data/ldap/repository/cdi/LdapRepositoryBean.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.ldap.repository.cdi;
+
+import java.lang.annotation.Annotation;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+
+import org.springframework.data.ldap.repository.support.LdapRepositoryFactory;
+import org.springframework.data.repository.cdi.CdiRepositoryBean;
+import org.springframework.data.repository.config.CustomRepositoryImplementationDetector;
+import org.springframework.ldap.core.LdapOperations;
+import org.springframework.util.Assert;
+
+/**
+ * {@link CdiRepositoryBean} to create LDAP repository instances.
+ *
+ * @author Mark Paluch
+ * @since 2.1
+ */
+public class LdapRepositoryBean<T> extends CdiRepositoryBean<T> {
+
+	private final Bean<LdapOperations> operations;
+
+	/**
+	 * Creates a new {@link LdapRepositoryBean}.
+	 *
+	 * @param operations must not be {@literal null}.
+	 * @param qualifiers must not be {@literal null}.
+	 * @param repositoryType must not be {@literal null}.
+	 * @param beanManager must not be {@literal null}.
+	 * @param detector detector for the custom {@link org.springframework.data.repository.Repository} implementations
+	 *          {@link CustomRepositoryImplementationDetector}, can be {@link Optional#empty()}.
+	 */
+	LdapRepositoryBean(Bean<LdapOperations> operations, Set<Annotation> qualifiers, Class<T> repositoryType,
+			BeanManager beanManager, Optional<CustomRepositoryImplementationDetector> detector) {
+
+		super(qualifiers, repositoryType, beanManager, detector);
+
+		Assert.notNull(operations, "LdapOperations bean must not be null!");
+		this.operations = operations;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.cdi.CdiRepositoryBean#create(javax.enterprise.context.spi.CreationalContext, java.lang.Class)
+	 */
+	@Override
+	protected T create(CreationalContext<T> creationalContext, Class<T> repositoryType,
+			Optional<Object> customImplementation) {
+
+		LdapOperations ldapOperations = getDependencyInstance(operations, LdapOperations.class);
+		LdapRepositoryFactory factory = new LdapRepositoryFactory(ldapOperations);
+
+		return customImplementation.map(o -> factory.getRepository(repositoryType, o))
+				.orElseGet(() -> factory.getRepository(repositoryType));
+	}
+}
---
+++ src/test/java/org/springframework/data/ldap/repository/cdi/CdiExtensionIntegrationTests.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.ldap.repository.cdi;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Collections;
+
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.se.SeContainerInitializer;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.data.ldap.config.DummyEntity;
+import org.springframework.ldap.core.LdapTemplate;
+
+/**
+ * Integration tests for {@link LdapRepositoryExtension}.
+ *
+ * @author Mark Paluch
+ */
+public class CdiExtensionIntegrationTests {
+
+	static SeContainer container;
+
+	@BeforeClass
+	public static void setUp() {
+
+		container = SeContainerInitializer.newInstance() //
+				.disableDiscovery() //
+				.addPackages(CdiExtensionIntegrationTests.class) //
+				.initialize();
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		container.close();
+	}
+
+	@Test // DATALDAP-5
+	public void bootstrapsRepositoryCorrectly() {
+
+		RepositoryClient client = container.select(RepositoryClient.class).get();
+		LdapTemplate ldapTemplateMock = client.getLdapTemplate();
+
+		DummyEntity entity = new DummyEntity();
+		when(ldapTemplateMock.findAll(DummyEntity.class)).thenReturn(Collections.singletonList(entity));
+
+		SampleRepository repository = client.getSampleRepository();
+
+		assertThat(repository).isNotNull();
+
+		repository.deleteAll();
+
+		verify(client.getLdapTemplate()).delete(entity);
+	}
+
+	@Test // DATALDAP-5
+	public void returnOneFromCustomImpl() {
+
+		RepositoryClient repositoryConsumer = container.select(RepositoryClient.class).get();
+		assertThat(repositoryConsumer.getSampleRepository().returnOne()).isEqualTo(1);
+	}
+}
