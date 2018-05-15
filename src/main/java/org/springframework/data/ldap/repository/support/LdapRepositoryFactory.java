66f5afce5e48a5f31866f11842cb9af8a99ee278
Oliver Gierke <ogierke@pivotal.io>
2018-05-15 11:11:50 +0200

    DATALDAP-73 - Adapt to SpEL extension API changes in Spring Data Commons.
    Related tickets: DATACMNS-1260.

--- src/main/java/org/springframework/data/ldap/repository/support/LdapRepositoryFactory.java
+++ src/main/java/org/springframework/data/ldap/repository/support/LdapRepositoryFactory.java
@@ -30,9 +30,9 @@ import org.springframework.data.repository.core.NamedQueries;
 import org.springframework.data.repository.core.RepositoryInformation;
 import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
-import org.springframework.data.repository.query.EvaluationContextProvider;
 import org.springframework.data.repository.query.QueryLookupStrategy;
 import org.springframework.data.repository.query.QueryLookupStrategy.Key;
+import org.springframework.data.repository.query.QueryMethodEvaluationContextProvider;
 import org.springframework.data.repository.query.RepositoryQuery;
 import org.springframework.lang.Nullable;
 import org.springframework.ldap.core.LdapOperations;
@@ -101,7 +101,7 @@ public class LdapRepositoryFactory extends RepositoryFactorySupport {
 	 */
 	@Override
 	protected Optional<QueryLookupStrategy> getQueryLookupStrategy(@Nullable Key key,
-			EvaluationContextProvider evaluationContextProvider) {
+			QueryMethodEvaluationContextProvider evaluationContextProvider) {
 		return Optional.of(queryLookupStrategy);
 	}
 
