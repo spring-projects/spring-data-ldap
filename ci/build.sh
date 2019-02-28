b5110b8fc49ff28e4dc52e8501acc9e042bb24c9
Greg Turnquist <gturnquist@pivotal.io>
2019-02-28 11:06:34 -0600

    DATALDAP-101 - Polishing.

--- ci/build.sh
+++ ci/build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 [[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
 
-spring_data_ldap_artifactory=$(pwd)/spring-data--ldap-artifactory
+spring_data_ldap_artifactory=$(pwd)/spring-data-ldap-artifactory
 
 rm -rf $HOME/.m2/repository/org/springframework/data 2> /dev/null || :
 
